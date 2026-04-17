094f81813b0e76813a61b05572367ce94c5d0e7b
KunhaoXiu <107525652+KunhaoXiu@users.noreply.github.com>
2026-04-17 05:20:17 +0000

    Deploying to gh-pages from @ KunhaoXiu/KunhaoXiu.github.io@4a180584710307a15c6b91b0c1ba7b00b6bc4807 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "Projects",
+          title: "projects",
           description: "My cool projects.",
           section: "Navigation",
           handler: () => {
