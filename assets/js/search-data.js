9d2691787bb4313cc758ceb41b27857ce55a54bc
KunhaoXiu <107525652+KunhaoXiu@users.noreply.github.com>
2026-04-17 05:26:05 +0000

    Deploying to gh-pages from @ KunhaoXiu/KunhaoXiu.github.io@a2edcdf18cbf9b40729f0e11a02993ecde3c7b12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "CV",
+          title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
